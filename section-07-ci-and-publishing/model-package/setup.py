3292b0a6c438f65f0eb3f30bbe93d9c2c79f18cc
aqeel ahamad <aqeel.ahamad@gmail.com>
2021-10-03 16:45:58 +0800

    added

--- section-07-ci-and-publishing/model-package/setup.py
+++ section-07-ci-and-publishing/model-package/setup.py
@@ -8,9 +8,9 @@ from setuptools import find_packages, setup
 # Package meta-data.
 NAME = 'tid-regression-model'
 DESCRIPTION = "Example regression model package from Train In Data."
-URL = "https://github.com/trainindata/testing-and-monitoring-ml-deployments"
-EMAIL = "christopher.samiullah@protonmail.com"
-AUTHOR = "ChristopherGS"
+URL = "https://github.com/aqeelahamad/deploying-machine-learning-models"
+EMAIL = "aqeel@protonmail.com"
+AUTHOR = "ChristopherGS-aqeel"
 REQUIRES_PYTHON = ">=3.6.0"
 
 
